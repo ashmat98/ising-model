2d5e233d18267fbfe607ead01caaf584ddbf63c1
Ashot Matevosyan <ashmat98@gmail.com>
2021-03-28 01:04:47 +0400

    notebooks

--- python/parallel.py
+++ python/parallel.py
@@ -2,9 +2,10 @@ from ising_model import Simulate_MH
 import numpy as np
 
 
-def to_run(steps):
-    engine = Simulate_MH(32, 32, 100, np.random.randint(0, 1000000))
-    engine.set_T(100)
+def to_run(i, steps, T, N, freq, SEED):
+    print(i)
+    engine = Simulate_MH(int(N), int(N), int(freq), int(SEED))
+    engine.set_T(T)
     engine.random_init()
-    engine.make_steps(steps)
+    engine.make_steps(int(steps))
     return (engine.get_sampled_M(), engine.get_sampled_E())
